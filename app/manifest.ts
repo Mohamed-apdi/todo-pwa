de7ed9d19c57eae4fba3bc69270b15cd64d75483
Mohamed <maxamedcabdifitaax33@gmail.com>
2025-08-13 12:14:46 +0300

    updated icon

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,9 +11,9 @@ export default function manifest(): MetadataRoute.Manifest {
     background_color: '#ffffff',
     theme_color: '#111827',
     icons: [
-      { src: '/icons/icon-192.png', sizes: '192x192', type: 'image/png' },
-      { src: '/icons/icon-512.png', sizes: '512x512', type: 'image/png' },
-      { src: '/icons/icon-512.png', sizes: '512x512', type: 'image/png', purpose: 'maskable' },
+      { src: '/todo/public/icon/icon-192.png', sizes: '192x192', type: 'image/png' },
+      { src: '/todo/public/icon/icon-512.png', sizes: '512x512', type: 'image/png' },
+      { src: '/todo/public/icon/icon-512.png', sizes: '512x512', type: 'image/png', purpose: 'maskable' },
     ],
   };
 }
